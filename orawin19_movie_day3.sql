88260bf20f417874ccedc2ee83c6b96dadf6b0c4
matthcol <matthias@learning64.net>
2024-10-09 15:35:04 +0200

    query examples

--- orawin19_movie_day3.sql
+++ orawin19_movie_day3.sql
@@ -13,13 +13,36 @@ create view movie_director as
     select m.id, m.title, m.year, m.duration_mn as duration, m.director_id, p.name
     from movies m left join persons p on m.director_id = p.id;
     
--- Exemple: vue en modification pour ajouter film annee en cours 
+-- Exercice: vue en modification pour ajouter film annee en cours 
 -- et modification uniquement colonnes duration_mn, synopsis, poster_uri
--- accorder ses droits � 1 nouvel utilisateur movieredactor
+-- accorder ses droits à 1 nouvel utilisateur movieredactor
 
+-- Query:
 select m.id, m.title, m.year, m.duration_mn, m.synopsis, m.poster_uri
 from movies m 
 where m.year = extract(year from sysdate);
+
+-- Tests
+insert into movies -- remplacer movies par le nom de la vue
+    (title, year) values ('Joker: Folie à deux', 2024);
+insert into movies (title, year, duration) 
+    values ('Beetlejuice Beetlejuice', 2024, 105);
+
+update movies -- ou nom de la vue
+set duration = 138
+where id = xxxxx;
+
+-- on veut interdire les suivantes:
+update movies -- ou nom de la vue
+set year = 2032
+where id = xxxxx;
+
+update movies -- ou nom de la vue
+set title = 'bad movie'
+where id = xxxxx;
+
+delete movies -- ou nom de la vue
+where id = xxxx;
 
 
 
